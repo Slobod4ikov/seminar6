d80b0c9f4e20374268f69d825d4d3b9eaaff6946
Oleg <slobodchikov_1990@mail.ru>
2023-04-05 22:19:38 +0300

    solved task 43

--- Program.cs
+++ Program.cs
@@ -4,18 +4,15 @@ y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2
 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
-double b1;
-Console.WriteLine("Vvedite pervoe 4islo dli pervoi przmoi");
-double.TryParse(Console.ReadLine(), out b1);
-double k1;
-Console.WriteLine("Vvedite vtoroe 4islo dli pervoi przmoi");
-double.TryParse(Console.ReadLine(), out k1);
-double b2;
-Console.WriteLine("Vvedite pervoe 4islo dli vtoroi przmoi");
-double.TryParse(Console.ReadLine(), out b2);
-double k2;
-Console.WriteLine("Vvedite vtoroe 4islo dli vtoroi przmoi");
-double.TryParse(Console.ReadLine(), out k2);
-double x = ((b2 - b1)/(k2-k1))/2;
-double y = k2*x+b2;
-Console.WriteLine($"To4ka perese4enii dvux primix: x={x}, y={y}");
+int b1, k1, b2, k2;
+Console.WriteLine("Введите В1: ");
+int.TryParse(Console.ReadLine(), out b1);
+Console.WriteLine("Введите К1: ");
+int.TryParse(Console.ReadLine(), out k1);
+Console.WriteLine("Введите В2: ");
+int.TryParse(Console.ReadLine(), out b2);
+Console.WriteLine("Введите К2: ");
+int.TryParse(Console.ReadLine(), out k2);
+double x = (b1 - b2)/(k2-k1);
+double y = k1*x+b1;
+Console.WriteLine($"Точка пересечения имеет координаты: x={x}, y={y}");
